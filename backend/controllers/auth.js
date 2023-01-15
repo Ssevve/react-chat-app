8507a8b68e5086bb41f20e24a7277768fb5b2c1e
Seweryn <ssevve@gmail.com>
2023-01-15 21:10:40 +0100

    Add logout server route

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -3,25 +3,6 @@ const bcrypt = require('bcrypt');
 const generateAccessToken = require('../helpers/generateAccessToken');
 const generateRefreshToken = require('../helpers/generateRefreshToken');
 
-const handleSignup = async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const duplicate = await User.findOne({ username });
-    if (duplicate) return res.status(409).json({ message: 'Username already taken' });
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = await User.create({
-      username,
-      password: hashedPassword,
-    });
-
-    res.status(201).json(newUser);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-};
-
 const handleLogin = async (req, res) => {
   const { username, password } = req.body;
   try {
@@ -41,7 +22,6 @@ const handleLogin = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       httpOnly: true,
       secure: true,
-      path: '/refresh',
     });
 
     res.status(200).json({
@@ -58,4 +38,47 @@ const handleLogin = async (req, res) => {
   }
 };
 
-module.exports = { handleSignup, handleLogin };
+const handleSignup = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const duplicate = await User.findOne({ username });
+    if (duplicate) return res.status(409).json({ message: 'Username already taken' });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      username,
+      password: hashedPassword,
+    });
+
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const handleLogout = async (req, res) => {
+  const cookies = req.cookies;
+  if (!cookies.refreshToken) return res.sendStatus(204);
+  const refreshToken = cookies.refreshToken;
+
+  // Check if the token is in the DB
+  const user = await User.findOne({ refreshToken });
+  if (!user) {
+    res.clearCookie('refreshToken', { httpOnly: true, secure: true });
+    return res.sendStatus(204);
+  }
+
+  // Delete refresh token from the db
+  user.refreshToken = '';
+  await user.save();
+
+  res.clearCookie('refreshToken', { httpOnly: true, secure: true });
+  res.sendStatus(204);
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+module.exports = { handleSignup, handleLogin, handleLogout };
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,5 +5,6 @@ const validateSignup = require('../middleware/validateSignup');
 
 router.post('/login', validateLogin, authController.handleLogin);
 router.post('/signup', validateSignup, authController.handleSignup);
+router.get('/logout', authController.handleLogout);
 
 module.exports = router;
