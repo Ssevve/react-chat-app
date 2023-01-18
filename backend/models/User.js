c1f6fddd572d2d63366136e3e45d4a1dbaf674c0
Seweryn <ssevve@gmail.com>
2023-01-18 17:59:14 +0100

    Change User model _id field to use uuid

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
 
 const userSchema = mongoose.Schema(
   {
+    _id: {
+      type: String,
+      default: uuidv4,
+    },
     username: {
       type: String,
       required: true,
