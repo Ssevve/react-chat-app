2757d128c18a10e784932615c2ad6df3456a2cbb
Seweryn <ssevve@gmail.com>
2023-01-18 21:46:00 +0100

    Add UserAvatar component

---
+++ frontend/src/components/UserAvatar.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+import avatarPlaceholder from '../assets/no-avatar.jpg';
+
+const Avatar = styled.img`
+  --size: ${(props) => props.size};
+  height: var(--size);
+  width: var(--size);
+  border-radius: 50%;
+`;
+
+function UserAvatar({ user, size, className, ...rest }) {
+  return (
+    <Avatar
+      size={size}
+      className={className}
+      src={user.avatar.url || avatarPlaceholder}
+      alt={user.username}
+    />
+  );
+}
+
+export default UserAvatar;
