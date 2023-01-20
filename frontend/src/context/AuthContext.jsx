2f28c7100b61dcd339a249c991da7056f46abf9d
Seweryn <ssevve@gmail.com>
2023-01-20 18:16:35 +0100

    Remove console log from AuthContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,13 +1,8 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useState } from 'react';
 
 export const AuthContext = createContext(null);
 
 export const AuthContextProvider = ({ children }) => {
   const [auth, setAuth] = useState(null);
-
-  useEffect(() => {
-    console.log(auth);
-  }, [auth]);
-
   return <AuthContext.Provider value={{ auth, setAuth }}>{children}</AuthContext.Provider>;
 };
