2f78d6a52d830dedcf2cab62e3fd4729767b0a30
Seweryn <ssevve@gmail.com>
2023-02-09 02:09:52 +0100

    Remove unused socket code

--- backend/sockets.js
+++ backend/sockets.js
@@ -1,6 +1,4 @@
 const { Server } = require('socket.io');
-const Message = require('./models/Message');
-const Chat = require('./models/Chat');
 
 const connectedUsers = {};
 
@@ -26,37 +24,6 @@ const initializeSocketEvents = (server, app) => {
     const { userId } = socket.handshake.auth;
     addUser(userId, socket.id);
     io.emit('receiveConnectedUsers', { users: connectedUsers });
-
-    socket.on('sendMessage', async ({ message, receiverId }) => {
-      const newMessage = await Message.create(message);
-      newMessage.populate('sender', 'username avatar.url');
-      let chat = await Chat.findOne({ _id: message.chatId });
-      if (!chat) {
-        chat = new Chat({
-          _id: message.chatId,
-          members: [message.sender, receiverId],
-        });
-      }
-
-      chat.lastMessage = newMessage._id;
-      await chat.save();
-
-      const newChats = await Chat.find({ members: { $in: receiverId } })
-        .sort({ createdAt: 1 })
-        .populate([
-          {
-            path: 'lastMessage',
-            select: 'content sender createdAt',
-          },
-          {
-            path: 'members',
-            select: 'username avatar.url',
-          },
-        ])
-        .exec();
-
-      io.to(connectedUsers[receiverId]).emit('receiveMessage', { newMessage, newChats });
-    });
 
     socket.on('disconnect', () => {
       removeUser(userId);
