3cd3aaa88da9a113f2d7b3f64e8e99f4ce6b2d7c
Seweryn Świrgoń <ssevve@gmail.com>
2023-02-12 23:27:01 +0100

    Add connection string env var

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,7 @@ mongoose.set('strictQuery', true);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.mkuarps.mongodb.net/chat-app?retryWrites=true&w=majority`,
-    );
+    await mongoose.connect(process.env.DB_CONNECTION_STRING);
   } catch (err) {
     console.error(err);
   }
