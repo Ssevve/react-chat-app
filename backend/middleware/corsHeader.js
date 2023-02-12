c02c4a92bc29f4b01087b5b46dfbc4423bb0ad32
Seweryn <ssevve@gmail.com>
2023-02-12 19:47:01 +0100

    Add more cors headers to incoming requests

--- backend/middleware/corsHeader.js
+++ backend/middleware/corsHeader.js
@@ -3,8 +3,14 @@ const allowedOrigins = ['http://localhost:3000', 'https://react-chat-app-khaki.v
 const corsHeader = (req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', origin);
   }
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  res.header('Access-Control-Allow-credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE');
   next();
 };
 
