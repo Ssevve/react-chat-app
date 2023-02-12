7d2cc61c0d493990c9cc41b57a2ac5c1fe43d6e9
Seweryn <ssevve@gmail.com>
2023-02-12 21:47:13 +0100

    Fix connection string

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ mongoose.set('strictQuery', true);
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.mkuarps.mongodb.net/chat-app?retryWrites=true&w=majority/`,
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.mkuarps.mongodb.net/chat-app?retryWrites=true&w=majority`,
     );
   } catch (err) {
     console.error(err);
