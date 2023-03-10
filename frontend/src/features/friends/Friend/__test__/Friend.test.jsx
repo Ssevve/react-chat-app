61dfddd04effb204498c2526f94291a29ccf3803
Seweryn Świrgoń <ssevve@gmail.com>
2023-03-10 19:12:23 +0100

    Add Friend tests

---
+++ frontend/src/features/friends/Friend/__test__/Friend.test.jsx
@@ -0,0 +1,44 @@
+import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import '__mocks__/matchMediaMock';
+import { renderWithProviders } from 'utils/testUtils';
+
+import Friend from '..';
+
+const testFriend = {
+  username: 'Friend',
+  avatar: {
+    url: '',
+  },
+};
+
+test("renders friend's avatar", () => {
+  renderWithProviders(<Friend friend={testFriend} />);
+  expect(screen.getByAltText('Friend')).toBeInTheDocument();
+});
+
+test("renders friend's connection status", () => {
+  renderWithProviders(<Friend friend={testFriend} />);
+  expect(screen.getByRole('note')).toBeInTheDocument();
+});
+
+test("renders friend's username", () => {
+  renderWithProviders(<Friend friend={testFriend} />);
+  expect(screen.getByText('Friend')).toBeInTheDocument();
+});
+
+test('renders "send message" button', () => {
+  renderWithProviders(<Friend friend={testFriend} />);
+
+  const regex = new RegExp(`message ${testFriend.username}`, 'i');
+
+  expect(screen.getByRole('button', { name: regex })).toBeInTheDocument();
+});
+
+test('renders "remove friend" button', () => {
+  renderWithProviders(<Friend friend={testFriend} />);
+
+  const regex = new RegExp(`remove ${testFriend.username}`, 'i');
+
+  expect(screen.getByRole('button', { name: regex })).toBeInTheDocument();
+});
