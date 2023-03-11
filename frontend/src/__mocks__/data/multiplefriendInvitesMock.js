e76c2cb587220636c5a52f8e808909e3163332c5
Seweryn Świrgoń <ssevve@gmail.com>
2023-03-11 13:28:08 +0100

    Add FriendInvites tests and mocks

---
+++ frontend/src/__mocks__/data/multiplefriendInvitesMock.js
@@ -0,0 +1,38 @@
+const multipleFriendInvitesMock = [
+  {
+    _id: '1',
+    sender: {
+      _id: '3',
+      username: 'FirstInviteSender',
+      avatar: {
+        url: '',
+      },
+    },
+    receiver: {
+      _id: '2',
+      username: 'FirstInviteReceiver',
+      avatar: {
+        url: '',
+      },
+    },
+  },
+  {
+    _id: '2',
+    sender: {
+      _id: '2',
+      username: 'SecondInviteSender',
+      avatar: {
+        url: '',
+      },
+    },
+    receiver: {
+      _id: '3',
+      username: 'SecondInviteReceiver',
+      avatar: {
+        url: '',
+      },
+    },
+  },
+];
+
+export default multipleFriendInvitesMock;
---
+++ frontend/src/__mocks__/data/singleFriendInviteMock.js
@@ -0,0 +1,21 @@
+const singleFriendInviteMock = [
+  {
+    _id: '1',
+    sender: {
+      _id: '3',
+      username: 'InviteSender',
+      avatar: {
+        url: '',
+      },
+    },
+    receiver: {
+      _id: '2',
+      username: 'InviteReceiver',
+      avatar: {
+        url: '',
+      },
+    },
+  },
+];
+
+export default singleFriendInviteMock;
